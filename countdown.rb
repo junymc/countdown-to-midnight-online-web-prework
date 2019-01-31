dfc0c98b01bb92c2b17253ac3d5c30b684c269d8
Junyoung McArdle <juny.mcardle@gmail.com>
2019-01-31 20:13:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,9 @@
 #write your code here
 
 def countdown(number)
-# counter = 10
-counter = number
   while number > 0
     puts "#{number} SECOND(s)!"
-    counter -= 1
+    number -= 1
   end
   return "HAPPY NEW YEAR!"
 end
